68cb823226a9cfef6122bf9fa08d3f121b691ec3
白章璞 <zhangpu.bai@edhec.com>
2020-02-25 17:07:03 +0100

    first version routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'recipes#index'
+
+  resources :profiles, only: :show
+
+  resources :recipes
+  resources :planners, only: [:show, :create, :update] do
+    resources :planner_recipes, only: [:destroy, :create]
+  end
+  resources :shopping_lists, only: [:index, :update]
+  resources :liked_recipes, only: [:index, :create, :destroy]
+  resources :ratings, only: :create
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

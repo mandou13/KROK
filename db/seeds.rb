b1fd940c2036b7dfaf6fa62b6efd52a87c9688a6
白章璞 <zhangpu.bai@edhec.com>
2020-02-25 23:47:04 +0100

    controller actions & view basics finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "destroy"
+Recipe.destroy_all
+User.destroy_all
+
+users = [
+  {
+    email: "pauline@me.com",
+    password: "azerty"
+  }
+]
+
+puts "create users"
+users.each do |user|
+  User.create!(user)
+end
+puts "#{User.count} users"
+
+recipes = [
+  {
+    name: "Apple Pie d'Automne",
+    description: "Préchauffer le four à 200°C (thermostat 6-7).",
+    dish_type: "Dessert",
+    dish_origin: "French",
+    prep_time: "50 minutes",
+    servings: 6
+  }
+]
+
+puts "create recipes"
+recipes.each do |recipe|
+  new_recipe = Recipe.new(recipe)
+  new_recipe.user = User.first
+  new_recipe.save!
+end
+puts "#{Recipe.count} recipes"

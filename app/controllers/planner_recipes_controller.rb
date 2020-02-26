87c3ebbbef7391a82f1cd30567cb0d97ec914cae
Louis Gatinet <louis.gatinet@gmail.com>
2020-02-26 19:52:59 +0100

    checked button done

---
+++ app/controllers/planner_recipes_controller.rb
@@ -0,0 +1,16 @@
+class PlannerRecipesController < ApplicationController
+
+  def create
+  end
+
+  def update
+    @planner = Planner.find(params[:planner_id])
+    @planner_recipe = PlannerRecipe.find(params[:id])
+    @planner_recipe.cooked = true
+    @planner_recipe.save
+    redirect_to planner_path(@planner)
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     resources :ratings, only: :create
   end
   resources :planners, only: [:show, :create, :update] do
-    resources :planner_recipes, only: [:destroy, :create]
+    resources :planner_recipes, only: [:destroy, :create, :update]
     resources :shopping_lists, only: [:index, :update]
   end
   resources :liked_recipes, only: [:index, :destroy]

2a33fd6ea1ec17f62dec295deba33c7f69249651
Maxence Bourbotte <maxencebourbotte@gmail.com>
2020-02-26 16:49:44 +0100

    Banner

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,1 +1,7 @@
-console.log('Hello')
+// console.log('Hello')
+
+// if (window.location.path_name = "/")
+
+// const logoHome = document.getElementById('logo-home')
+// logoHome.classList.add('active')
+

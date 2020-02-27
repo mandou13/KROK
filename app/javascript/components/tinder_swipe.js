dbe16dd15053c655f428d4a9ad6be81baff6bf99
Louis Gatinet <louis.gatinet@gmail.com>
2020-02-27 18:51:00 +0100

    hammer install

--- app/javascript/components/tinder_swipe.js
+++ app/javascript/components/tinder_swipe.js
@@ -1,23 +1,3 @@
-import "bootstrap";
-import "components/navbar";
-import "packs/plugin/profile.js";
-
-import { btnHeartToggle } from "components/btn_heart_toggle";
-import { initLikedRecipeNav } from "components/init_like_recipe_nav";
-import { display_add_or_random } from '../components/add_or_random'
-import { display_title } from '../components/planner_title'
-import { display_searchbar } from '../components/searchbar_index_recipe.js'
-import { display_filter } from '../components/searchbar_index_recipe.js'
-
-display_title();
-display_searchbar();
-display_filter();
-btnHeartToggle();
-initLikedRecipeNav();
-display_add_or_random();
-
-
-
 'use strict';
 
 var tinderContainer = document.querySelector('.tinder');
@@ -115,4 +95,3 @@ var loveListener = createButtonListener(true);
 
 nope.addEventListener('click', nopeListener);
 love.addEventListener('click', loveListener);
-

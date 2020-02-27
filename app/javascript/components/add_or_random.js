3aca57837be983b394c65045fc1b03090f205db7
Louis Gatinet <louis.gatinet@gmail.com>
2020-02-27 21:48:42 +0100

    page done except 2 little issues on refreshing

--- app/javascript/components/add_or_random.js
+++ app/javascript/components/add_or_random.js
@@ -1,3 +1,4 @@
+
 const display_add_or_random = () => {
 
 const card_to_display = document.querySelector('#card_to_display')
@@ -5,21 +6,41 @@ const card_to_hide = document.querySelector('#card_to_hide')
 const card_update_launch = document.querySelector('#card-img-icon-add')
 const back_to_card_through_search = document.querySelector('#card-img-icon-search')
 const back_to_card_through_random = document.querySelector('#card-img-icon-random')
+const card_img_icon_random_valid = document.querySelector('#card-img-icon-random-valid')
+const card_img_icon_random_again = document.querySelector('#card-img-icon-random-again')
+const card_random_to_hide = document.querySelector('#card_random_to_hide')
 
-if (card_to_display && card_to_hide &&card_update_launch && back_to_card_through_search && back_to_card_through_random) {
+if (
+  card_to_display &&
+  card_to_hide &&
+  card_update_launch &&
+  back_to_card_through_search &&
+  back_to_card_through_random &&
+  card_img_icon_random_valid
+  ) {
   card_update_launch.addEventListener('click', (event) => {
-    card_to_display.classList.remove("d-none")
-    card_to_hide.classList.add("d-none")
+    card_to_display.classList.remove("d-none");
+    card_to_hide.classList.add("d-none");
   })
 
   back_to_card_through_search.addEventListener('click', (event) => {
-    card_to_display.classList.add("d-none")
-    card_to_hide.classList.remove("d-none")
+    card_to_display.classList.add("d-none");
+    card_to_hide.classList.remove("d-none");
   })
 
   back_to_card_through_random.addEventListener('click', (event) => {
-    card_to_display.classList.add("d-none")
-    card_to_hide.classList.remove("d-none")
+    card_to_display.classList.add("d-none");
+    card_to_hide.classList.remove("d-none");
+    card_random_to_hide.classList.remove("d-none");
+
+   card_img_icon_random_again.addEventListener('click', (event) => {
+    card_random_to_hide.classList.remove("d-none");
+   })
+
+  });
+  card_img_icon_random_valid.addEventListener('click', (event) => {
+    card_random_to_hide.classList.add("d-none");
+    // location.reload();
   })
   }
 

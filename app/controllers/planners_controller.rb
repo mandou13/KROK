bd65106c7d83b1e9e22c94dcdd2255dcaab584f4
Louis Gatinet <louis.gatinet@gmail.com>
2020-02-28 00:23:54 +0100

    for the 3 categories : starter, main, dessert

--- app/controllers/planners_controller.rb
+++ app/controllers/planners_controller.rb
@@ -1,8 +1,24 @@
 class PlannersController < ApplicationController
   def show
     @recipes = Recipe.all
+    @recipes_main = Recipe.where(dish_type: 'main')
+    @recipes_starter = Recipe.where(dish_type: 'starter')
+    @recipes_dessert = Recipe.where(dish_type: 'dessert')
+    # @recipes_appetizer = Recipe.where(dish_type: 'appetizer')
+    # @recipes_side = Recipe.where(dish_type: 'side')
+    # @recipes_snack = Recipe.where(dish_type: 'snack')
+    # @recipes_other = @recipes_appetizer + @recipes_side + @recipes_snack
+    @recipes_all = [@recipes_starter, @recipes_main, @recipes_dessert]
     @planner = current_user.planners.last
     @planner_recipes = @planner.planner_recipes.sort
+    @planner_recipes_main = @planner.planner_recipes.select { |planner_recipe| planner_recipe.recipe.dish_type == 'main'}.sort
+    @planner_recipes_starter = @planner.planner_recipes.select { |planner_recipe| planner_recipe.recipe.dish_type == 'starter'}.sort
+    @planner_recipes_dessert = @planner.planner_recipes.select { |planner_recipe| planner_recipe.recipe.dish_type == 'dessert'}.sort
+    # @planner_recipes_appetizer = @planner.planner_recipes.select { |planner_recipe| planner_recipe.recipe.dish_type == 'appetizer'}.sort
+    # @planner_recipes_side = @planner.planner_recipes.select { |planner_recipe| planner_recipe.recipe.dish_type == 'side'}.sort
+    # @planner_recipes_snack = @planner.planner_recipes.select { |planner_recipe| planner_recipe.recipe.dish_type == 'snack'}.sort
+    # @planner_recipes_other = @planner_recipes_appetizer + @planner_recipes_side + @planner_recipes_snack
+    @planner_recipes_all = [@planner_recipes_starter, @planner_recipes_main, @planner_recipes_dessert]
     @rating = Rating.new
   end
 
@@ -27,3 +43,6 @@ class PlannersController < ApplicationController
     params.require(:planner).permit(:name)
   end
 end
+
+
+# dish_type, inclusion: { in: ['dessert', 'starter', 'appetizer', 'side', 'snack', 'main'] }

f144de983dfa1d84acef87254cfb0f858b930083
Louis Gatinet <louis.gatinet@gmail.com>
2020-02-27 16:55:50 +0100

    solving javascript issue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 import "bootstrap";
-import { display_title } from './plugin/planner_title'
 import "components/navbar";
 import { display_add_or_random } from '../components/add_or_random'
+import { display_title } from '../components/planner_title'
 
 display_title();
 display_add_or_random();

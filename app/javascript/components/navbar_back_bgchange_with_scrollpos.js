44969fe28ef365a8acc1e2fd82bf9bc20bd55258
白章璞 <zhangpu.bai@edhec.com>
2020-02-28 10:36:05 +0100

    navback js added

---
+++ app/javascript/components/navbar_back_bgchange_with_scrollpos.js
@@ -0,0 +1,16 @@
+const navBackChangeWithScrollPos = () => {
+  const navBack = document.querySelector('.navbar-back');
+  let last_scroll_position = 0;
+  if (navBack) {
+    navBack.addEventListener('scroll', (event) => {
+      last_scroll_position = window.scrollY;
+      let bottomPos = navBack.getBoundingClientRect().top + last_scroll_position;
+        if (bottomPos >= 463) {
+          navBack.style.backgroundColor = "white";
+        } else {
+          navBack.style.backgroundColor = "transparent";
+        }
+    })
+  }
+}
+export { navBackChangeWithScrollPos };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,11 @@ import "packs/plugin/profile.js";
 
 import { btnHeartToggle } from "components/btn_heart_toggle";
 import { initLikedRecipeNav } from "components/init_like_recipe_nav";
-import { display_add_or_random } from '../components/add_or_random'
-import { display_title } from '../components/planner_title'
-import { display_searchbar } from '../components/searchbar_index_recipe.js'
-import { display_filter } from '../components/searchbar_index_recipe.js'
+import { display_add_or_random } from '../components/add_or_random';
+import { display_title } from '../components/planner_title';
+import { display_searchbar } from '../components/searchbar_index_recipe.js';
+import { display_filter } from '../components/searchbar_index_recipe.js';
+import { navBackChangeWithScrollPos } from '../components/navbar_back_bgchange_with_scrollpos';
 
 display_title();
 display_searchbar();
@@ -15,3 +16,4 @@ display_filter();
 btnHeartToggle();
 initLikedRecipeNav();
 display_add_or_random();
+navBackChangeWithScrollPos();

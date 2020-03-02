1326737c4a41dcec3b7338b2a3f81fa8beb2cb7e
Maxence Bourbotte <maxencebourbotte@gmail.com>
2020-03-02 19:05:36 +0100

    Preference filtering

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,6 +1,6 @@
 class Preference < ApplicationRecord
   belongs_to :user
-  belongs_to :ingredient
+  belongs_to :ingredient, dependent: :destroy
   validates_uniqueness_of :user_id, scope: :ingredient_id
 
 end

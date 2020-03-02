1f21574bafc110fb4b7d2934c219c0202ceb52c2
白章璞 <zhangpu.bai@edhec.com>
2020-03-02 11:07:15 +0100

    debug btnHeart

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,6 @@ import "bootstrap";
 import "components/navbar";
 import "packs/plugin/profile.js";
 
-import { btnHeartToggle } from "components/btn_heart_toggle";
 import { initLikedRecipeNav } from "components/init_like_recipe_nav";
 import { display_add_or_random } from '../components/add_or_random';
 import { display_title } from '../components/planner_title';
@@ -14,7 +13,6 @@ import { btnAddRecipeToggle } from "components/btn_addrecipe_toggle";
 display_title();
 display_searchbar();
 display_filter();
-btnHeartToggle();
 initLikedRecipeNav();
 btnAddRecipeToggle();
 

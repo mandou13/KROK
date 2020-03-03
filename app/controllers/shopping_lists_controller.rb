3d5ebd1dc8df8faa6dcf18b742fd0b383c82e748
Louis Gatinet <louis.gatinet@gmail.com>
2020-03-03 13:38:01 +0100

    list with all ingredients in alphabetical order

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -8,5 +8,10 @@ class ShoppingListsController < ApplicationController
   end
 
   def update
+    @planner        = Planner.find(params[:planner_id])
+    @shopping_item  = ShoppingList.find(params[:id])
+    @shopping_item.bought = !@shopping_item.bought
+    @shopping_item.save
+    redirect_to planner_shopping_lists_path(@planner)
   end
 end

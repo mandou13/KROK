defdf3b8714552f671dba410f010f416fabcab88
Amandine Legal <amandine.legal@edhec.com>
2020-03-03 17:43:01 +0100

    fixing bugs, adding quantity from index

--- app/controllers/planner_recipes_controller.rb
+++ app/controllers/planner_recipes_controller.rb
@@ -1,10 +1,10 @@
 class PlannerRecipesController < ApplicationController
 
   def create
-    @planner_recipe = PlannerRecipe.new(set_planner_recipe_params)
+    @recipe = Recipe.find(params[:recipe_id])
+    @planner_recipe = PlannerRecipe.new(servings: @recipe.servings, cooked: false)
     @planner = current_user.planners.last
     @planner_recipe.planner = @planner
-    @recipe = Recipe.find(params[:recipe_id])
     @planner_recipe.recipe = @recipe
 
     if @planner_recipe.save
@@ -47,11 +47,29 @@ class PlannerRecipesController < ApplicationController
   end
 
   def update
+
     @planner = Planner.find(params[:planner_id])
     @planner_recipe = PlannerRecipe.find(params[:id])
-    @planner_recipe.cooked = !@planner_recipe.cooked
+
+    @planner_recipe.cooked = !@planner_recipe.cooked if params[:query] == 'cooked'
+    @planner_recipe.servings += 1 if params[:query] == 'plus'
+    @planner_recipe.servings -= 1 if params[:query] == 'minus'
     if @planner_recipe.save
+      @planner_recipe.recipe.ingredients.each do |ingredient|
+        shopping_lists = ShoppingList.where("planner_id = ? AND ingredient_name = ?", @planner.id, ingredient.name)
+
+        quotient = @planner_recipe.servings.to_i.fdiv(@planner_recipe.recipe.servings.to_i)
+        ingredient.quantity = (quotient * ingredient.quantity)
+        ingredient = ingredient.convert_ingredient_to_krok_unit unless ingredient.krok_unit?
+        shopping_list_reference = shopping_lists.first
+        shopping_list_reference.quantity = ingredient.quantity
+        shopping_list_reference.save!
+      end
       redirect_to planner_path(@planner)
+      # respond_to do  |format|
+      #     format.html{redirect_to planner_path(@planner)}
+      #     format.js { render  'planner_recipes.js.erb'}
+      #   end
     end
   end
 

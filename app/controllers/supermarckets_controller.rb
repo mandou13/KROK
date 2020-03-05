cd5e8d50e7479032c49fa91ef131aa9fb980f5a4
Louis Gatinet <louis.gatinet@gmail.com>
2020-03-05 16:50:56 +0100

    api key secured

--- app/controllers/supermarckets_controller.rb
+++ app/controllers/supermarckets_controller.rb
@@ -10,8 +10,7 @@ class SupermarcketsController < ApplicationController
     @markers = []
     @markers_open_only = []
     supermarcket_types.each do |supermarcket_type|
-      url_supermarcket_type = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=#{supermarcket_type}&location=#{coord}&radius=10000&key=AIzaSyCci3T2TVK23rtk8I6cpjXUrhgyCWEPz4w"
-      # url_supermarcket_type = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=#{supermarcket_type}&location=#{coord}&radius=10000&key=#{ENV['GOOGLE_API_PLACES_KEY']}"
+      url_supermarcket_type = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=#{supermarcket_type}&location=#{coord}&radius=10000&key=#{ENV['GOOGLE_API_PLACES_KEY']}"
       supermarckets_type_serialized = open(url_supermarcket_type).read
       @supermarckets_type = JSON.parse(supermarckets_type_serialized)["results"]
       @markers_supermarcket_type = @supermarckets_type.map do |supermarcket|

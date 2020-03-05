f66a15288a714c500e5462a9133013c30d06e4b0
白章璞 <zhangpu.bai@edhec.com>
2020-03-05 12:58:21 +0100

    search doublons removed & opacity condiction corrected

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,7 +17,6 @@ import { update_servings } from '../components/recipe_servings';
 import { update_servings_planner } from '../components/recipe_servings_planner';
 import { handleClickOnTab } from '../components/handleClickOnTab';
 import { plannerSearchClick } from './plugin/plannerSearchSimulateClick';
-// import { handleColorBtnAddOldPlanner } from '../components/handleColorBtnAddOldPlanner';
 
 display_title();
 display_searchbar();
@@ -37,4 +36,3 @@ update_servings();
 update_servings_planner();
 handleClickOnTab();
 plannerSearchClick();
-// handleColorBtnAddOldPlanner();

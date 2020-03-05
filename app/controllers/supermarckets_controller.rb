7e137be12558b10421b0af67faf323d78a667feb
Louis Gatinet <louis.gatinet@gmail.com>
2020-03-05 13:12:14 +0100

    work for all supermarckets given

--- app/controllers/supermarckets_controller.rb
+++ app/controllers/supermarckets_controller.rb
@@ -8,19 +8,24 @@ class SupermarcketsController < ApplicationController
     url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=50.630125,3.013894&radius=800000&keyword=Grocery store&key=AIzaSyCci3T2TVK23rtk8I6cpjXUrhgyCWEPz4w'
     supermarckets_serialized = open(url).read
     @supermarckets = JSON.parse(supermarckets_serialized)["results"]
-    supermarcket_type = "carrefour"
-    url2 = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=#{supermarcket_type}&location=50.630125,3.013894&radius=800000&key=AIzaSyCci3T2TVK23rtk8I6cpjXUrhgyCWEPz4w"
-    supermarckets_serialized2 = open(url2).read
-    @supermarckets2 = JSON.parse(supermarckets_serialized2)["results"]
-    # @supermarcket_markers = []
-    @markers = @supermarckets2.map do |supermarcket|
-      {
-        lat: supermarcket["geometry"]["location"]["lat"],
-        lng: supermarcket["geometry"]["location"]["lng"],
-        infoWindow: render_to_string(partial: "info_window", locals: { supermarcket: supermarcket }),
-        image_url: helpers.asset_url("#{supermarcket_type}.png")
-      }
+    supermarcket_types = ["carrefour", "monoprix", "lidl", "auchan"]
+    @markers = []
+    supermarcket_types.each do |supermarcket_type|
+      url2 = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=#{supermarcket_type}&location=50.630125,3.013894&radius=800000&key=AIzaSyCci3T2TVK23rtk8I6cpjXUrhgyCWEPz4w"
+      supermarckets_serialized2 = open(url2).read
+      @supermarckets2 = JSON.parse(supermarckets_serialized2)["results"]
+      @markers_supermarcket_type = @supermarckets2.map do |supermarcket|
+        {
+          lat: supermarcket["geometry"]["location"]["lat"],
+          lng: supermarcket["geometry"]["location"]["lng"],
+          infoWindow: render_to_string(partial: "info_window", locals: { supermarcket: supermarcket }),
+          image_url: helpers.asset_url("#{supermarcket_type}.png")
+        }
+      end
+      @markers << @markers_supermarcket_type
     end
+      # @markers = @markers.map(&:first)
+      @markers = @markers.first + @markers.second + @markers.third + @markers.fourth
   end
 end
 
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -19,8 +19,9 @@ const addMarkersToMap = (map, markers) => {
       element.className = 'marker';
       element.style.backgroundImage = `url('${marker.image_url}')`;
       element.style.backgroundSize = 'contain';
-      element.style.width = '35px';
-      element.style.height = '35px';
+      element.style.backgroundRepeat = "repeat-x"
+      element.style.width = '45px';
+      element.style.height = '45px';
     new mapboxgl.Marker(element)
       .setLngLat([ marker.lng, marker.lat ])
       .setPopup(popup)
